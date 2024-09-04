c6ef69310e6e4b651ec8c5de8ddc2ecdd598e530
mayor-of-geology <atolagbejoshua2@gmail.com>
2024-09-04 17:40:52 +0100

    modify train

--- train.py
+++ train.py
@@ -10,6 +10,11 @@ if torch.cuda.is_available():
     torch.backends.cudnn.deterministic = True
 np.random.seed(random_seed)
 
+v8_small = '/content/drive/My Drive/MSc Project/faulty_insulator/yolov8s_100aug_True/train_val_wo_sahi/weights/best.pt'
+v8_large = '/content/drive/My Drive/MSc Project/faulty_insulator/yolov8x_100aug_True/train_val_wo_sahi/weights/best.pt'
+v9 = '/content/drive/My Drive/MSc Project/faulty_insulator/yolov9_100aug_True/train_val_wo_sahi/weights/best.pt'
+v10 = '/content/drive/My Drive/MSc Project/faulty_insulator/yolov10_100aug_True/train_val_wo_sahi/weights/best.pt'
+
 if __name__ == '__main__':
      
     parser = ArgumentParser(description='Hyperparameters')
@@ -20,26 +25,47 @@ if __name__ == '__main__':
                         help='Number of epochs')
     parser.add_argument('--aug', nargs='?', type=bool, default=False,
                         help='Whether to use data augmentation.')
-    parser.add_argument('--model_scale', nargs='?', type=str, default='ela',
+    parser.add_argument('--model_scale', nargs='?', type=str, default='ela-large',
                         help='model scale type')
     parser.add_argument('--name', nargs='?', type=str, default='yolov8',
                         help='name')
     args = parser.parse_args()
 
-    if args.model_scale == 'gam':
-       model = YOLO('ultralytics/cfg/models/v8/yolov8s_GAM.yaml').load('/content/drive/My Drive/MSc Project/faulty_insulator/yolov8s_100aug_True/train_val_wo_sahi/weights/best.pt')
-
-    elif args.model_scale == 'res-cbam':
-        model = YOLO('ultralytics/cfg/models/v8/yolov8s_ResBlock_CBAM.yaml').load('/content/drive/My Drive/MSc Project/faulty_insulator/yolov8s_100aug_True/train_val_wo_sahi/weights/best.pt')
-
-    elif args.model_scale == 'ela':
-        model = YOLO('ultralytics/cfg/models/v8/yolov8s_ELA.yaml').load('/content/drive/My Drive/MSc Project/faulty_insulator/yolov8s_100aug_True/train_val_wo_sahi/weights/best.pt')
+    if args.model_scale == 'gam-small':
+       model = YOLO('ultralytics/cfg/models/v8/yolov8s_GAM.yaml').load(v8_small)
+       
+    elif args.model_scale == 'gam-large':
+       model = YOLO('ultralytics/cfg/models/v8/yolov8x_GAM.yaml').load(v8_large)
     
-    elif args.model_scale == 'sa':
-        model = YOLO('ultralytics/cfg/models/v8/yolov8s_SA.yaml').load('/content/drive/My Drive/MSc Project/faulty_insulator/yolov8s_100aug_True/train_val_wo_sahi/weights/best.pt')
+    elif args.model_scale == 'cbam-small':
+        model = YOLO('ultralytics/cfg/models/v8/yolov8s_CBAM.yaml').load(v8_small)
     
-    elif args.model_scale == 'eca':
-        model = YOLO('ultralytics/cfg/models/v8/yolov8s_ECA.yaml').load('/content/drive/My Drive/MSc Project/faulty_insulator/yolov8s_100aug_True/train_val_wo_sahi/weights/best.pt')
+    elif args.model_scale == 'cbam-large':
+        model = YOLO('ultralytics/cfg/models/v8/yolov8x_CBAM.yaml').load(v8_large)
+    
+    elif args.model_scale == 'rescbam-small':
+        model = YOLO('ultralytics/cfg/models/v8/yolov8s_ResBlock_CBAM.yaml').load(v8_small)
+        
+    elif args.model_scale == 'rescbam-large':
+        model = YOLO('ultralytics/cfg/models/v8/yolov8s_ResBlock_CBAM.yaml').load(v8_large)
+
+    elif args.model_scale == 'ela-small':
+        model = YOLO('ultralytics/cfg/models/v8/yolov8s_ELA.yaml').load(v8_small)
+        
+    elif args.model_scale == 'ela-large':
+        model = YOLO('ultralytics/cfg/models/v8/yolov8x_ELA.yaml').load(v8_large)
+    
+    elif args.model_scale == 'sa-small':
+        model = YOLO('ultralytics/cfg/models/v8/yolov8s_SA.yaml').load(v8_small)
+    
+    elif args.model_scale == 'sa-large':
+        model = YOLO('ultralytics/cfg/models/v8/yolov8x_SA.yaml').load(v8_large)
+    
+    elif args.model_scale == 'eca-small':
+        model = YOLO('ultralytics/cfg/models/v8/yolov8s_ECA.yaml').load(v8_small)
+        
+    elif args.model_scale == 'eca-large':
+        model = YOLO('ultralytics/cfg/models/v8/yolov8x_ECA.yaml').load(v8_large)
     
     elif args.model_scale == 'yolov9':
         model = YOLO("https://github.com/ultralytics/assets/releases/download/v8.2.0/yolov9e.pt")
@@ -71,9 +97,9 @@ if __name__ == '__main__':
         flipud=0.5,
         shear=0.5,
         translate=0.4,
-        mosaic= .5, #you can modify
-        mixup= 0.15, #you can modify
-        copy_paste= 0.3, #you can modify,
+        mosaic=0.5, #you can modify
+        mixup=0.15, #you can modify
+        copy_paste=0.3, #you can modify,
         scale=0.9, #you can modify
     )
 
